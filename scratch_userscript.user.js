d113f62a5c1c170fe12c714c27de26a6ebbb494f
provigz (Vankata453) <78196474+provigz@users.noreply.github.com>
2026-08-24 20:35:49 +0300

    Update userscript to prompt user after page load when project tries to use HTTP hack
    TODO: Listen when a new project is loaded in the Scratch editor as to reset "hasConfirmedThisSession".

--- scratch_userscript.user.js
+++ scratch_userscript.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Scratch HTTP GET requests hack
 // @namespace    https://github.com/provigz/ScratchYouTube
-// @version      2025-12-13
-// @description  A hack that allows Scratch projects to perform HTTP GET requests to a certain host. Used by the provigz/ScratchYouTube project.
+// @version      2026-08-24
+// @description  A hack that allows Scratch projects to perform HTTP GET requests to a certain host, localhost by default. Used by the provigz/ScratchYouTube project.
 // @author       provigz
 // @match        https://scratch.mit.edu/projects/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=scratch.mit.edu
@@ -15,19 +15,38 @@ const targetHost = "http://localhost";
 (function() {
     'use strict';
 
+    let hasConfirmedThisSession = false;
+
     const originalFetch = unsafeWindow.fetch;
     unsafeWindow.fetch = async function(req, options) {
-        if (typeof req.url === "string") {
-            const text = (new URLSearchParams(req.url)).get("text")
-            if (text.startsWith("HTTP ")) {
-                if (req.url.startsWith("https://translate-service.scratch.mit.edu/translate")) {
-                    return originalFetch(`${targetHost}/translate?text=${text.substring(5)}`, options);
-                } else if (req.url.startsWith("https://synthesis-service.scratch.mit.edu/synth")) {
-                    return originalFetch(`${targetHost}/synth?text=${text.substring(5)}`, options);
+        if (typeof req.url === "string")
+        {
+            if (req.url.startsWith("https://translate-service.scratch.mit.edu/translate") ||
+                req.url.startsWith("https://synthesis-service.scratch.mit.edu/synth"))
+            {
+                const text = (new URLSearchParams(req.url)).get("text");
+                if (text && text.startsWith("HTTP "))
+                {
+                    if (!hasConfirmedThisSession) {
+                        const userApproved = unsafeWindow.confirm(
+                            `The current Scratch project is requesting to connect to "${targetHost}" to fetch custom data.\n\nMAKE SURE YOU TRUST THIS PROJECT! Do you wish to proceed?`
+                        );
+                        if (userApproved) {
+                            hasConfirmedThisSession = true;
+                        } else {
+                            return originalFetch(req, options);
+                        }
+                    }
+
+                    // Proceed with the redirection if confirmed
+                    if (req.url.includes("translate")) {
+                        return originalFetch(`${targetHost}/translate?text=${text.substring(5)}`, options);
+                    } else {
+                        return originalFetch(`${targetHost}/synth?text=${text.substring(5)}`, options);
+                    }
                 }
             }
         }
         return originalFetch(req, options);
     };
-
 })();
